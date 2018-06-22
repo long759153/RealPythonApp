086202815f95591a5661063df9d7d8e86e57c3ab
AlanCheng <mr.coffee@outlook.com>
2018-06-22 14:29:38 +0800

    just another try for update
    Signed-off-by: long759153 <mr.coffee@outlook.com>

--- scratch.py
+++ scratch.py
@@ -4,6 +4,11 @@ def greater():
 def newGreater():
     print('hello, again')
 
+def greaterSomeone(name):
+    print('hello,'+ name)
 
+name_1 = 'Lion'
 greater()
 newGreater()
+
+greaterSomeone(name_1)
